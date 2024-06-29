93d6bbc0600ae7e5802def3e6c0464f0a7dab3c8
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-06-29 13:26:31 +0530

    Add Instrument model

---
+++ models/instrument.js
@@ -0,0 +1,20 @@
+const { default: mongoose } = require("mongoose");
+
+const mongoose = require(mongoose);
+
+const Schema = mongoose.Schema;
+
+const InstrumentSchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+  description: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+  price: { type: Number, required: true },
+  image: { type: String },
+  number: { type: Number, required: true },
+});
+
+InstrumentSchema.virtual("url").get(function () {
+  return `/catalog/instrument/${this._id}`;
+});
+
+module.exports = mongoose.model("Instrument", InstrumentSchema);
