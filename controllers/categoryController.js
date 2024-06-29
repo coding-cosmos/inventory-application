fec6d18e1ba02c1500da2e7ff67681fb2679d21e
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-06-29 18:35:16 +0530

    Implement category_list controller method

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,7 +3,12 @@ const asyncHandler = require('express-async-handler');
 
 // Display list of all Categories.
 exports.category_list = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category list");
+    const categories = await Category.find({}).sort({ name: 1 }).exec();
+
+    res.render('category_list',{
+      active:"category",
+      categories:categories
+    })
   });
   
   // Display detail page for a specific Category.
