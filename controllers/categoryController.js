16fcdcf366e032d10d58a37a523ea2a6f667f5d7
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-06-29 19:32:01 +0530

    Implement category_detail controller method

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,47 +1,57 @@
-const Category = require('../models/category');
-const asyncHandler = require('express-async-handler');
+const Category = require("../models/category");
+const Instrument = require("../models/instrument");
+const asyncHandler = require("express-async-handler");
 
 // Display list of all Categories.
 exports.category_list = asyncHandler(async (req, res, next) => {
-    const categories = await Category.find({}).sort({ name: 1 }).exec();
+  const categories = await Category.find({}).sort({ name: 1 }).exec();
 
-    res.render('category_list',{
-      active:"category",
-      categories:categories
-    })
+  res.render("category_list", {
+    active: "category",
+    categories: categories,
   });
-  
-  // Display detail page for a specific Category.
-  exports.category_detail = asyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: Category detail: ${req.params.id}`);
+});
+
+// Display detail page for a specific Category.
+exports.category_detail = asyncHandler(async (req, res, next) => {
+  const category = await Category.findById(req.params.id).exec();
+  const instruments = await Instrument.find({ category: req.params.id },"name image")
+    .sort({ name: 1 })
+    .exec();
+
+  res.render("category_detail", {
+    active: "category",
+    category: category,
+    instruments: instruments,
   });
-  
-  // Display Category create form on GET.
-  exports.category_create_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category create GET");
-  });
-  
-  // Handle Category create on POST.
-  exports.category_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category create POST");
-  });
-  
-  // Display Category delete form on GET.
-  exports.category_delete_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category delete GET");
-  });
-  
-  // Handle Category delete on POST.
-  exports.category_delete_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category delete POST");
-  });
-  
-  // Display Category update form on GET.
-  exports.category_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category update GET");
-  });
-  
-  // Handle Category update on POST.
-  exports.category_update_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category update POST");
-  });
+});
+
+// Display Category create form on GET.
+exports.category_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Category create GET");
+});
+
+// Handle Category create on POST.
+exports.category_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Category create POST");
+});
+
+// Display Category delete form on GET.
+exports.category_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Category delete GET");
+});
+
+// Handle Category delete on POST.
+exports.category_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Category delete POST");
+});
+
+// Display Category update form on GET.
+exports.category_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Category update GET");
+});
+
+// Handle Category update on POST.
+exports.category_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Category update POST");
+});
