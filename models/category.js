1fbcee195964c312fff40537436bf3312d7abfbf
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-06-29 13:25:01 +0530

    Add category model

---
+++ models/category.js
@@ -0,0 +1,16 @@
+const { default: mongoose, mongo } = require("mongoose");
+
+const mongoose = require(mongoose);
+
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+  description: { type: String, required: true },
+});
+
+CategorySchema.virtual("url").get(function () {
+  return `/catalog/category/${this._id}`;
+});
+
+module.exports = mongoose.Model("Category", CategorySchema);
