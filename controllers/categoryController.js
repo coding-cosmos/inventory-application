3c6279c77edffdf881aeb8d51590bf1838bb657b
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-06-30 16:38:00 +0530

    Implement create and update controller methods

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,8 @@
 const Category = require("../models/category");
 const Instrument = require("../models/instrument");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
 
 // Display list of all Categories.
 exports.category_list = asyncHandler(async (req, res, next) => {
@@ -28,13 +30,34 @@ exports.category_detail = asyncHandler(async (req, res, next) => {
 
 // Display Category create form on GET.
 exports.category_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category create GET");
+  res.render("category_form",{ title: "Create New Category",active:"category",category:undefined,errors:null});  
 });
 
 // Handle Category create on POST.
-exports.category_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category create POST");
-});
+exports.category_create_post = [
+  body("name","Category name must not be empty.")
+  .trim()
+  .isLength({min:1})
+  .escape(),
+
+  body("description","Description must not be empty")
+  .trim()
+  .isLength({min:1})
+  .escape(),
+
+  asyncHandler(async (req,res,next)=>{
+    const errors = validationResult(req);
+    
+    const category = new Category({name:req.body.name,description:req.body.description});
+
+    if(errors.isEmpty()){
+      await category.save();
+      res.render("category_detail",{active:"category",category:category,instruments:undefined});
+    }else{
+      res.render("category_form",{title:"Create New Category",active:"category",category:category,errors:errors.array()});
+    }
+  }),
+];
 
 // Display Category delete form on GET.
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
@@ -48,10 +71,34 @@ exports.category_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display Category update form on GET.
 exports.category_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category update GET");
+  const category = await Category.findById(req.params.id).exec();
+
+  res.render("category_form",{title:"Update Category",active:"category",category:category,errors:null}); 
 });
 
 // Handle Category update on POST.
-exports.category_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category update POST");
-});
+exports.category_update_post = [
+  body("name","Category name must not be empty.")
+  .trim()
+  .isLength({min:1})
+  .escape(),
+
+  body("description","Description must not be empty")
+  .trim()
+  .isLength({min:1})
+  .escape(),
+
+  asyncHandler(async (req,res,next)=>{
+    const errors = validationResult(req);
+    
+    const category = new Category({name:req.body.name,description:req.body.description,_id:req.params.id});
+
+    if(errors.isEmpty()){
+     const updatedCategory = await Category.findByIdAndUpdate(req.params.id,category,{});
+
+     res.redirect(updatedCategory.url);
+    }else{
+      res.render("category_form",{title:"Create New Category",active:"category",category:category,errors:errors.array()});
+    }
+  }),
+];
