d066973ac9ef08515cd84dd7365d68d9b9d8efd2
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-07-02 14:58:07 +0530

    Extract MongoDB connection string to env variable

--- app.js
+++ app.js
@@ -15,10 +15,8 @@ const app = express();
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const dev_db_url =
-  "mongodb+srv://sujalmittal2000:JXWKTfBHg9gs4Au3@cluster0.2t5iy5n.mongodb.net/inventory-application?retryWrites=true&w=majority&appName=Cluster0";
 
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI;
 
 main().catch((err) => console.log(err));
 async function main() {
